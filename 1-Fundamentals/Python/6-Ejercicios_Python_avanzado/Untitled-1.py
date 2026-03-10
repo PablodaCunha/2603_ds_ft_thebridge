5eccf64276d9b1c50ff74c11af61d496d3ac16ee
Pablo da Cunha <91959436+PabloGlu@users.noreply.github.com>
2026-03-10 17:30:14 +0100

    Numpy

--- 1-Fundamentals/Python/6-Ejercicios_Python_avanzado/Untitled-1.py
+++ 1-Fundamentals/Python/6-Ejercicios_Python_avanzado/Untitled-1.py
@@ -1,14 +1,9 @@
-def devolver(lista):
-    lista1 = []
-    for x in lista:
-        if x == 'rojo':
-            lista1.append('stop')
-            return 
-        if x == 'verde':
-            lista1.append(['avance'])
-        if x == 'amarillo':
-            lista1.append(['precaución'])
-    print(lista1)
-    return lista1
+import numpy as np
 
-devolver(['rojo', 'verde', 'amarillo'])
+print(np.__version__)
+
+
+# help(np)
+# np.random
+array = np.array([1,2,3])
+print(type(array))
